a86d106a5ede12d4ffb7ec4e75ce77c3622c30bc
marceloll <marcelo.lopes.lotufo@gmail.com>
2020-06-13 12:13:45 -0300

    Fix conversar callback

--- fluxos/conversar/index.js
+++ fluxos/conversar/index.js
@@ -1,10 +1,10 @@
 const { clearMarkup } = require("../../util");
 
 function init(config, bot) {
-  bot.onText(/\/conversar/i, oiCallback, clearMarkup);
+  bot.onText(/\/conversar/i, oiCallback(bot), clearMarkup);
 }
 
-const oiCallback = (msg, match) => {
+const oiCallback = (bot) => (msg, match) => {
   const chatId = msg.chat.id;
 
   bot.sendMessage(chatId, `Fala meu chapa ${msg.from.first_name}`);
