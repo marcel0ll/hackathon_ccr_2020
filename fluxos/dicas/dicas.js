9de371a44f21b041aa6b3b9d0450e37485f07878
marceloll <marcelo.lopes.lotufo@gmail.com>
2020-06-14 16:18:44 -0300

    Add health tips

---
+++ fluxos/dicas/dicas.js
@@ -0,0 +1,75 @@
+const dicas = [
+  [
+    `ALIMENTE-SE BEM`,
+    `Diminua alimentos industrializados, ricos em sal, açúcares e gorduras. De prioridade a
+alimentos que não precisa abrir embalagem. Inclua frutas e não se esqueça das
+saladas. Quanto mais colorida, melhor!`,
+  ],
+
+  [
+    `RESPIRE PROFUNDAMENTE`,
+    `Respire fundo, expandindo o seu peito. Solte lentamente o ar. Faça isso várias vezes ao
+dia. A respiração profunda diminui o estresse.`,
+  ],
+
+  [
+    `CONVERSE COM UM PSICÓLOGO`,
+    `Se pensa que é preciso estar com alguma doença mental para procurar um Psicólogo,
+está enganado! Busque ajuda desse profissional ao perceber dificuldades em lidar com
+alguns sentimentos, frustrações, alterações no sono e no humor.  Ele irá te ajudar a
+cuidar disso!`,
+  ],
+
+  [
+    `ALONGUE-SE`,
+    `Esse exercício ajuda a desenvolver a flexibilidade, eliminar encurtamentos musculares,
+auxilia na recuperação da fadiga muscular, melhora a consciência corporal, previne
+dores, diminuir a tensão e o estresse.`,
+  ],
+
+  [
+    `BEBA MUITA ÁGUA`,
+    `Não espere até que esteja com sede antes de pegar um copo de água. Manter-se
+hidratado durante todo o dia pode ajudá-lo a ficar energizado e suprimir sentimentos
+de fome. A água ajuda a drenar nossos sistemas de resíduos e toxinas.`,
+  ],
+
+  [
+    `EVITE O EXCESSO DE ESTRESSE`,
+    `Excessopode causar alterações hormonais e prejudicar gravemente o`,
+    `funcionamento do seu organismo. O estresse contribui para a depressão, que é um
+enorme problema de saúde hoje. Cuide-se!`,
+  ],
+
+  [
+    `ABANDONE VÍCIOS QUE PREJUDICAM SUA SAÚDE`,
+    `Vícios em drogas, medicamentos, bebidas alcoólicas e cigarros afetam diretamente sua
+saúde e bem-estar! Portanto, abandoná-los é a única saída. Se precisar, procure
+tratamento especializado.`,
+  ],
+
+  [
+    `PRATIQUE EXERCÍCIO FÍSICO`,
+    `Quando você se exercita faz com que seu corpo, cérebro e hormônios funcionem
+melhor.
+Praticar exercícios pode ajudar a reduzir a depressão e o risco de várias doenças
+crônicas, como obesidade, diabetes tipo 2, doenças cardíacas, prevenir o Alzheimer e
+muitas outras.`,
+  ],
+
+  [
+    `CONVIVA COM AS PESSOAS QUE AMA`,
+    `É muito importante trabalhar, mas aproveitar de um tempo ao lado das pessoas que
+ama, faz com que a vida tenha sentido e cor. E lembre-se: o que vale é a qualidade
+desse tempo, e não necessariamente a quantidade de horas que terá disponível.`,
+  ],
+
+  [
+    `PRIORIZE UM BOM SONO`,
+    `Você precisa dormir quantas horas para realmente se sentir descansado? Cada um terá
+uma resposta para essa pergunta. O importante é conseguir dormir e descansar. Isso é
+fundamental para o relaxamento do corpo e da mente.`,
+  ],
+];
+
+module.exports = dicas;
