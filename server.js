119aa51b3fb2401d211640ec47cfe483d52585a2
marceloll <marcelo.lopes.lotufo@gmail.com>
2020-06-14 02:36:32 -0300

    use express

--- server.js
+++ server.js
@@ -1,35 +1,9 @@
 // This is a health checker http server
 // if this is running, the bot is probably healthy
 // if this is down, the bot is probably down
+const express = require("express");
+const PORT = process.env.PORT || 8080;
 
-const http = require("http");
-
-function getHandler(req, res, reqUrl) {
-  res.writeHead(200);
-  res.write("OK");
-  res.end();
-}
-
-function noResponse(req, res) {
-  res.writeHead(404);
-  res.end();
-}
-
-http
-  .createServer((req, res) => {
-    const router = {
-      "GET/": getHandler,
-      default: noResponse,
-    };
-
-    let reqUrl = new URL(req.url, process.env.HOST);
-
-    let redirectedFunc =
-      router[req.method + reqUrl.pathname] || router["default"];
-    redirectedFunc(req, res, reqUrl);
-  })
-  .listen(process.env.PORT || 8080, () => {
-    console.log(
-      "Server is running at " + process.env.HOST + (process.env.PORT || 8080)
-    );
-  });
+express()
+  .get("/", (req, res) => res.send("ok"))
+  .listen(PORT, () => console.log(`Listening on ${PORT}`));
